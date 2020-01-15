2e18e5466b729d5502bb5930356eb79235a2eb75
Samantha Farls <sa764671@ucf.edu>
2020-01-15 12:06:35 -0500

    #51 in class demo

--- ice/w2a/index.js
+++ ice/w2a/index.js
@@ -1,5 +1,5 @@
 function scopeDemo() {
-    console.log(x)
+    //console.log(x)
     let a = 3, b = 4, c = 5;
     console.log(a);
     console.log(b);
@@ -12,7 +12,7 @@ function scopeDemo() {
 
 function runOnLoad()
 {
-    console.log(x);
+    //console.log(x);
     var submitButton = document.getElementById("submitButton");
     submitButton.addEventListener("click",onClick);
     var submitButton = document.getElementById("");
