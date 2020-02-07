db28eabf7e58f39f0d561e8df136f77ca2fc56c6
sfarls <sfarls2018@knights.ucf.edu>
2020-02-07 18:08:21 -0500

    lab 2

---
+++ labs/2/src/components/card/index.js
@@ -0,0 +1,17 @@
+import "./index.css"
+
+class Card {
+    constructor(props) {
+        this.props = props;
+        this.element = document.createElement("div");
+        this.element.innerHTML = this.props.content;
+        this.element.className = "card";
+
+    }
+
+    render() {
+        return this.element;
+    }
+}
+
+export default Card;
